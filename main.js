ad4ce689ab22b797b89f80f1072157d89a38c788
DrakeEquehours <DrakeEquehours@users.noreply.github.com>
2021-03-29 03:08:19 +0700

    clock

--- main.js
+++ main.js
@@ -103,3 +103,17 @@ function checkEmail(){
     icon1.style.display = "block"
   }
 }
+//clock
+const deg = 6;
+const hr = document.querySelector('#hr')
+const mn = document.querySelector('#mn')
+const sc = document.querySelector('#sc')
+setInterval(()=> {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+  hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+})
